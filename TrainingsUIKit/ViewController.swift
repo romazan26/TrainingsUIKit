78b16a35e25705068ce2cfe92d4d2334ef073dd7
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-12-15 22:28:03 +0300

    Initial Commit

---
+++ TrainingsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingsUIKit
+//
+//  Created by Роман on 15.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
